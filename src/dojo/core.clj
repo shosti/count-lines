16dbb2bacecd0ad12cef1fb1eac6744e1b042165
Emanuel Evans <emanuel.evans@gmail.com>
2013-12-18 20:02:25 -0800

    Do some preprocessing

--- src/dojo/core.clj
+++ src/dojo/core.clj
@@ -3,6 +3,13 @@
 
 (defn count-lines [str]
   (-> str
-     (s/split #"\n")
+     (s/split-lines)
+     (strip-whitespace)
+     (remove-blank-lines)
      (count)))
 
+(defn strip-whitespace [lines]
+  (map s/trim lines))
+
+(defn remove-blank-lines [lines]
+  (filter #(not (s/blank? %)) lines))
